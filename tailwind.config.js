870f9d9bfefc001f08e19066780c389f64dd8ff4
Cat <ccallcottstevens@gmail.com>
2021-05-03 10:35:59 +1000

    reconfigured tailwind to only purge in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+console.log(process.env.NODE_ENV);
+const purge = process.env.NODE_ENV == 'production' ? true: false;
 module.exports = {
-  purge: [],
+  purge: { enabled: purge, content: ['/build/**/*.html']},
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
