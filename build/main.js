dbaaacee099dca601dc0d243e0c9c65a1654ee85
Cat <ccallcottstevens@gmail.com>
2021-05-04 22:11:04 +1000

    changing html using DOM

---
+++ build/main.js
@@ -0,0 +1,10 @@
+// EXAMINE THE DOCUMENT OBJECT
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.all);//array(actually an HTML collection) of every element in the DOM - access all of these by their index number!
+
+let headerTitle = document.getElementById('header-title');
+console.log(headerTitle.innerText);
