5b21c5b88b78b5cb0b1fd108ad313f49e13df503
Cat <ccallcottstevens@gmail.com>
2021-05-06 11:10:48 +1000

    added event listener for adding elements to list

--- build/main.js
+++ build/main.js
@@ -6,22 +6,35 @@
 // console.log(document.head);
 // console.log(document.all);//array(actually an HTML collection) of every element in the DOM - access all of these by their index number!
 
-let headerTitle = document.getElementById('header-title');
-console.log(headerTitle.innerText);
+// let headerTitle = document.getElementById('header-title');
+// console.log(headerTitle.innerText);
 
-//QuerySelector - only one item(grabs the first one)
-let header = document.querySelector('#main-box');
-header.style.borderBottom = 'solid 4px #ccc';
+// //QuerySelector - only one item(grabs the first one)
+// let header = document.querySelector('#main-box');
+// header.style.borderBottom = 'solid 4px #ccc';
 
 
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for (let i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#D1D5DB';
-}
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// for (let i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = '#D1D5DB';
+// }
 
-let button = document.getElementById('button').addEventListener('click', buttonClick)
+// let button = document.getElementById('button').addEventListener('click', buttonClick)
 
-function buttonClick(e){
+// function buttonClick(e){
 
-}
+// }
+
+// let itemInput = document.querySelector('input[type="text"]');
+// let form = document.querySelector('form');
+
+// itemInput.addEventListener('keydown', runEvent)
+
+// function runEvent(e){
+//     console.log('Event Type: '+e.type);
+
+//     console.log(e.target.value);
+// }
+
+
